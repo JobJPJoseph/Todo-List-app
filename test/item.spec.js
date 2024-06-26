39ecad31bcf2eb82347e951ab10e596315eed9f8
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-06-26 14:57:50 -0400

    We are laying the ground work for the project

---
+++ test/item.spec.js
@@ -0,0 +1,11 @@
+const Item = require('../lib/item');
+
+const chai = require('chai');
+
+const spies = require('chai-spies');
+
+const expect = chai.expect;
+
+chai.use(spies);
+
+// Make sure to update "main" in your package.json file so that there is no issue running your tests.
