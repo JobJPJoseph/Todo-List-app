a3410ad78292e750dcfa084c54442bc03fd5df12
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-06-27 08:53:18 -0400

    We made test specs for the Item.isValidDate()

--- README.md
+++ README.md
@@ -10,8 +10,14 @@
     Single Responsibiity Principle:
     Custom Errors:
     Handling Errors:
-    Must use ES5 and ES6 Syntax:
     Linked Lists:
     Stacks and Queues:
 
 ### Item
+    Constructor
+        We need to have 3 properties: title, deadline and description. With deadline, we need to test if the
+        deadline is in the valid format. Note: We can't call instance methods before initialization but class instances we can.
+
+    validDate
+        The purpose of this method is to test if the deadline is in a proper format. Meaning we need to test the length, make sure it includes two dashes and includes nothing but numbers.
+            Based on the old code, there is a time complexity issue.
--- test/item.spec.js
+++ test/item.spec.js
@@ -9,3 +9,41 @@ const expect = chai.expect;
 chai.use(spies);
 
 // Make sure to update "main" in your package.json file so that there is no issue running your tests.
+
+describe('Item class', function () {
+
+    it('the Item class should exist', function () {
+        expect(Item).to.exist;
+    });
+
+    describe("validDate", function () {
+        const wrongType = 12345;
+        const lessThanTenLetters = 'abcdef';
+        const noDash = 'abcdefghei';
+        const withDash = 'b5-15-3s1d';
+        const correctFormat = '01-09-1198';
+
+
+        it('should return false when argument is not a string type', function () {
+            expect(Item.isValidDate(wrongType)).to.be.false;
+        });
+
+        it('should return false when its not the correct length', function () {
+            expect(Item.isValidDate(lessThanTenLetters)).to.be.false;
+        });
+
+        it('should return false when count of dashes does not equal two', function () {
+            expect(Item.isValidDate(noDash)).to.be.false;
+        });
+
+        it('should return false when the elements in between the dashes are not integers', function () {
+            expect(Item.isValidDate(withDash)).to.be.false;
+        });
+
+        it('should return true when properly formated', function () {
+            expect(Item.isValidDate(correctFormat)).to.be.true;
+        });
+
+    });
+
+});
